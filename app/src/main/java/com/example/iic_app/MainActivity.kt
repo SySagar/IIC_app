4b66542cedc2de5ba281d2ef45c44f8bc45496ab
SySagar <sysagar07@gmail.com>
2022-05-25 12:32:30 +0530

    added the basic coding without the popup window

--- app/src/main/java/com/example/iic_app/MainActivity.kt
+++ app/src/main/java/com/example/iic_app/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.iic_app
 
+import android.content.Context
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.transition.Slide
+import android.transition.TransitionManager
+import android.view.Gravity
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.AdapterView
+import android.widget.*
 import android.widget.AdapterView.OnItemSelectedListener
-import android.widget.ArrayAdapter
-import android.widget.EditText
-import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -22,6 +26,8 @@ class MainActivity : AppCompatActivity(), OnItemSelectedListener {
     var dm: Int=0
     var dc: Int=0
     var prop_choice=0
+    var results : TextView = findViewById(R.id.text_view)
+
 
     var propellants = arrayOf<String?>("Potassium Nitrate(KN)- 70%  Sucrose(SU)-30%",
         "Potassium Nitrate(KN)- 65%  Sorbitol(SB)-10% Sucrose(SU)- 24% Carbon-1%",
@@ -93,6 +99,79 @@ class MainActivity : AppCompatActivity(), OnItemSelectedListener {
         if(coreType=="round")
         {dc=(len.getText().toString()).toInt()}
 
+
+
+
+        //generating the output
+        val button : Button=findViewById(R.id.button)
+        button.setOnClickListener {
+            // Initialize a new layout inflater instance
+            val inflater: LayoutInflater =
+                getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+            // Inflate a custom view using layout inflater
+            val view = inflater.inflate(R.layout.another_view, null)
+
+            // Initialize a new instance of popup window
+            val popupWindow = PopupWindow(
+                view, // Custom view to show in popup window
+                LinearLayout.LayoutParams.WRAP_CONTENT, // Width of popup window
+                LinearLayout.LayoutParams.WRAP_CONTENT // Window height
+            )
+
+            // Set an elevation for the popup window
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                popupWindow.elevation = 10.0F
+            }
+
+
+            // If API level 23 or higher then execute the code
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                // Create a new slide animation for popup window enter transition
+                val slideIn = Slide()
+                slideIn.slideEdge = Gravity.TOP
+                popupWindow.enterTransition = slideIn
+
+                // Slide animation for popup window exit transition
+                val slideOut = Slide()
+                slideOut.slideEdge = Gravity.RIGHT
+                popupWindow.exitTransition = slideOut
+            }
+
+            // Get the widgets reference from custom view
+            val tv = view.findViewById<TextView>(R.id.text_view)
+            val buttonPopup = view.findViewById<Button>(R.id.button_popup)
+
+            // Set click listener for popup window's text view
+            tv.setOnClickListener {
+                // Change the text color of popup window's text view
+                tv.setTextColor(Color.RED)
+            }
+
+            // Set a click listener for popup's button widget
+            buttonPopup.setOnClickListener {
+                // Dismiss the popup window
+                popupWindow.dismiss()
+            }
+
+            // Set a dismiss listener for popup window
+            popupWindow.setOnDismissListener {
+                Toast.makeText(applicationContext, "Popup closed", Toast.LENGTH_SHORT).show()
+            }
+
+
+            // Finally, show the popup window on app
+
+            TransitionManager.beginDelayedTransition(root_layout)
+            popupWindow.showAtLocation(
+                root_layout, // Location to display popup window
+                Gravity.CENTER, // Exact position of layout to display popup
+                0, // X offset
+                0 // Y offset
+            )
+
+
+        }
     }
 
     override fun onItemSelected(parent: AdapterView<*>?,
@@ -343,7 +422,7 @@ class MainActivity : AppCompatActivity(), OnItemSelectedListener {
                     }
         }
 
-
+        results . setText(str)
 
     }
 }
